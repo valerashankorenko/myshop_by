a0e0fdbf0ac329c985837143a4579df4b4717376
Valeron <valerashankorenko@yandex.by>
2024-12-28 14:26:34 +0300

    Настройка админ панели

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import ParentCategory, Category, Product
 
-admin.site.register(ParentCategory)
-admin.site.register(Category)
-admin.site.register(Product)
+
+@admin.register(ParentCategory)
+class ParentCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
+    ordering = ('id',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'parent')
+    list_display_links = ('id', 'name', 'parent')
+    search_fields = ('id', 'name', 'parent__name')
+    ordering = ('parent',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'stock', 'category')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name', 'category__name')
+    ordering = ('id', 'category',)
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+
 from .models import User
 
 admin.site.unregister(Group)
@@ -7,16 +8,40 @@ admin.site.unregister(Group)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    exclude = ('is_staff', 'is_superuser', 'groups', 'user_permissions')
-    list_display = (
-        'id', 'first_name', 'last_name', 'phone_number',
-        'email', 'date_joined_format', 'is_active'
+    exclude = (
+        'is_staff',
+        'is_superuser',
+        'groups',
+        'user_permissions'
     )
-    list_display_links = ('email', 'date_joined_format')
-    search_fields = ('email', 'first_name', 'last_name')
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'email',
+        'date_joined_format',
+        'is_active'
+    )
+    list_display_links = (
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'date_joined_format'
+    )
+    search_fields = (
+        'email',
+        'first_name',
+        'last_name',
+        'phone_number'
+    )
+    ordering = ('id',)
     empty_value_display = '-пусто-'
 
     def date_joined_format(self, obj):
-        return obj.date_joined.strftime('%d-%m-%Y %H:%M')
+        return obj.date_joined.strftime(
+            '%d-%m-%Y %H:%M'
+        )
 
     date_joined_format.short_description = 'Дата регистрации'
